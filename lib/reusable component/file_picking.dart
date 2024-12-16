cf851c2f4523e09abd7535ef6a70321ed9573722
Abstract Programmer <AbstractProgrammer@example.com>
2024-12-16 14:42:28 +0530

    icon addition

--- lib/reusable component/file_picking.dart
+++ lib/reusable component/file_picking.dart
@@ -18,18 +18,24 @@ class FilePickerRow extends StatelessWidget {
         width: double.infinity,
         padding: EdgeInsets.all(10),
         decoration:
-            BoxDecoration(color: const Color.fromARGB(255, 157, 196, 228)),
+            BoxDecoration(color: const Color.fromARGB(255, 199, 228, 253)),
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              Icon(
+                Icons.file_copy,
+                color: Colors.grey.shade600,
+              ),
               Padding(
                 padding: const EdgeInsets.only(left: 5),
                 child: Text(
                   fileName,
                   style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 49, 46, 46)),
                   textAlign: TextAlign.center,
                 ),
               ),
@@ -40,7 +46,8 @@ class FilePickerRow extends StatelessWidget {
                   onPressed: onPickFile,
                   child: const Text(
                     "फाइल निवडा",
-                    style: TextStyle(fontSize: 18, color: Colors.black),
+                    style: TextStyle(
+                        fontSize: 18, color: Color.fromARGB(255, 46, 43, 43)),
                   ),
                 ),
               ),
