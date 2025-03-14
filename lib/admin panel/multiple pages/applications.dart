bef32ef1534a7774a81d84ef261e505ab4c84d8b
Abstract Programmer <AbstractProgrammer@example.com>
2025-03-14 19:05:25 +0530

    modification in appications.dart

--- lib/admin panel/multiple pages/applications.dart
+++ lib/admin panel/multiple pages/applications.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../reusable component/card_component.dart';
 import '../admindrawer.dart';
 import 'applied_applications/birth_certificates.dart';
